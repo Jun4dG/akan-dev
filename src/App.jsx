36ce8495b9cc43c05a7c4ac74c0af3abecd31cbe
Junford Gairanan <junfordm04@gmail.com>
2025-09-26 10:40:10 +0800

    removed footer each layout

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,6 @@ function App() {
             element={
               <ProtectedRoute>
                 <MainLayout />
-                <Footer />
               </ProtectedRoute>
             }
           />
